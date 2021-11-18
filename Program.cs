007ff73b06957464f1daec6436635c057315ee0f
Piotr Justyna <piotr.justyna@gmail.com>
2021-11-18 16:58:44 +0000

    Basic experiments.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using NLua;
 
 namespace LuaInterop
 {
@@ -6,6 +9,22 @@ namespace LuaInterop
     {
         static void Main(string[] args)
         {
+            var luaString1 =
+                "luanet.load_assembly \"System\"" +
+                "Console = luanet.import_type \"System.Console\"" +
+                "Math = luanet.import_type \"System.Math\"" +
+                "Directory = luanet.import_type \"System.IO.Directory\"" +
+                "Console.WriteLine(\"we are at {0}\",Directory.GetCurrentDirectory())" +
+                "Console.WriteLine(\"sqrt(2) is {0}\",Math.Sqrt(2))";
+
+            var luaString2 = File.ReadAllText("socket.lua");
+
+            using (Lua lua = new Lua())
+            {
+                lua.State.Encoding = Encoding.UTF8;
+                lua.DoString(luaString2);
+            }
+            
             Console.WriteLine("Hello World!");
         }
     }
